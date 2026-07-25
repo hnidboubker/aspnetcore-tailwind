53c58dd25fde058b672465205d51cc4f91cb500d
Houssine <nidboubkerhoussine@live.fr>
2026-07-25 16:08:29 +0200

    add maui hybride template

---
+++ Src/TailwindMaui.Web/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TailwindMaui.Web
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
