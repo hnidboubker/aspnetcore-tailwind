59fd64f4f3052fe266f38bf65cbb907f64777467
Houssine <nidboubkerhoussine@live.fr>
2026-08-15 23:09:14 +0200

    Updates: 2026-08-15T21:09:08.302Z

---
+++ Src/TailwindMvc.Web/Controllers/LegalController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TailwindMvc.Web.Controllers
+{
+    public class LegalController : Controller
+    {
+        public IActionResult CGU()
+        {
+            return View();
+        }
+        public IActionResult CGV()
+        {
+            return View();
+        } 
+        public IActionResult Confidentiality()
+        {
+            return View();
+        }
+        public IActionResult RGPD()
+        {
+            return View();
+        }
+    }
+}
